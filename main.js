95343fdc98297bb57384a9e7efeac3bea20caafc
goncharukvalera <goncharukvalera@gmail.com>
2019-04-11 22:47:30 +0300

    импорт всего модуля как объекта

--- main.js
+++ main.js
@@ -1,8 +1,4 @@
-// https://codesandbox.io
+import * as calc from "./math"; // импорт всех сущностей из модуля как объекта
 
-import {sum as s, divide as d, multyply as m} from "./math"; // импорт отдельных сущностей из модуля
-
-console.log(sum(2, 3));
-console.log(s(2, 3));
-console.log(d(8, 2));
-console.log(m(2, 2));
+console.log(calc.add(1, 2));
+console.log(calc.Pi);
