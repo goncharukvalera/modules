c5306ae58354368b67045a1903e54680cc23758e
goncharuk.valera <goncharukvalera@gmail.com>
2019-04-11 22:25:24 +0300

    импорт отдельных сущностей из модуля

---
+++ main.js
@@ -0,0 +1,5 @@
+// https://codesandbox.io
+
+import {sum, divide} from "./math"; // импорт отдельных сущностей из модуля
+
+console.log(sum(2, 3));
---
+++ math.js
@@ -0,0 +1,20 @@
+function sum(a, b) {
+    return a + b;
+}
+function substract(a, b) {
+    return a - b;
+}
+function multyply(a, b) {
+    return a * b;
+}
+function divide(a, b) {
+    return a / b;
+}
+class Graph {
+    addNode() {
+        console.log('node added');
+    }
+}
+const Pi = 3.1415;
+
+export {sum, divide, substract, multyply};
