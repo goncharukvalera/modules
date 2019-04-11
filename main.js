a3bffcad1c5f185853ae0ebe9c78edfee6c28a11
goncharukvalera <goncharukvalera@gmail.com>
2019-04-11 23:06:55 +0300

    импорт модуля который только выполняет что-то

--- main.js
+++ main.js
@@ -1,8 +1,1 @@
-// можно совмещать
-import Gr1, {divide, sub} from "./math";
-// или даже так
-import Gr1, * as calc from "./math";
-
-console.log(typeof Gr1);
-console.log(sub(5, 4));
-console.log(calc.sum(2, 5));
+import './action';
